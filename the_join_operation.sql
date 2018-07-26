fdc62c548364a653e78fb24f33c604d5114c57cc
Sun Kim <sunkim924@gmail.com>
2018-07-26 15:51:26 -0400

    add more solutions to JOIN

--- the_join_operation.sql
+++ the_join_operation.sql
@@ -1,15 +1,29 @@
-/* 1.  Modify [the query] to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER' */
+-- 6 The JOIN Operation: https://sqlzoo.net/wiki/The_JOIN_operation
+
+
+/* 1.
+Modify [the query] to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
+*/
 SELECT matchid,player FROM goal
-  WHERE teamid = 'GER';
+WHERE teamid = 'GER';
 
-
-/* 2. From the previous query you can see that Lars Bender's scored a goal in game 1012. Now we want to know what teams were playing in that match.
-
-Notice in the that the column matchid in the goal table corresponds to the id column in the game table. We can look up information about game 1012 by finding that row in the game table.
-
-Show id, stadium, team1, team2 for just game 1012 */
-SELECT DISTINCT id,stadium,team1,team2
-  FROM game
+/* 2.
+Show id, stadium, team1, team2 for just game 1012
+*/
+SELECT DISTINCT id,stadium,team1,team2 FROM game
 JOIN goal ON game.id=goal.matchid
 WHERE matchid=1012;
 
+/* 3.
+Modify it to show the player, teamid, stadium and mdate for every German goal.
+*/
+SELECT player,teamid,stadium,mdate
+FROM game JOIN goal ON (id=matchid)
+WHERE teamid='GER';
+
+/* 4.
+Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+*/
+SELECT team1,team2,player
+FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%'

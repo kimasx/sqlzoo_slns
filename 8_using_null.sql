b596ed18ea81db36908b733822fcab7638a988f6
Sun Kim <sunkim924@gmail.com>
2018-07-27 11:40:41 -0400

    add null

---
+++ 8_using_null.sql
@@ -0,0 +1,47 @@
+-- 8 SELECT basics
+-- https://sqlzoo.net/wiki/Using_Null
+
+
+/* 1.
+List the teachers who have NULL for their department.
+*/
+SELECT name FROM teacher
+WHERE dept IS NULL;
+
+/* 2.
+Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+*/
+SELECT teacher.name, dept.name
+FROM teacher INNER JOIN dept
+ON (teacher.dept=dept.id);
+
+/* 3.
+Use a different JOIN so that all teachers are listed.
+*/
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+ON (teacher.dept=dept.id);
+
+/* 4.
+Use a different JOIN so that all departments are listed.
+*/
+SELECT teacher.name, dept.name
+FROM dept LEFT JOIN teacher
+ON (teacher.dept=dept.id)
+
+/* 5.
+Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+*/
+SELECT name,
+       CASE WHEN COALESCE(mobile) IS NULL THEN '07986 444 2266'
+       ELSE mobile
+       END
+FROM teacher;
+
+
+
+
+
+
+
+
